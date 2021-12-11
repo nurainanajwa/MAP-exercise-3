003af4929047a89e9161c27503db4796aa717a5d
Nuraina Najwa <nurainanajwa@graduate.utm.my>
2021-12-12 00:57:31 +0800

    commit

--- lib/screens/task_list/bar.dart
+++ lib/screens/task_list/bar.dart
@@ -1,8 +1,12 @@
 // TODO Complete this file. This class should receive data from the TaskListScreen
 //
 import 'package:flutter/material.dart';
+import '../../models/todo.dart';
 
 class Bar extends StatelessWidget implements PreferredSizeWidget {
+  final Todo todoList;
+  Bar(this.todoList);
+
   @override
   Size get preferredSize => Size.fromHeight(50.0);
 
@@ -10,7 +14,7 @@ class Bar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       leading: Container(),
-      title: Text('Task list of Todo '),
+      title: Text('Task list of Todo' + todoList.title),
       centerTitle: true,
     );
   }
