94cd1622382fcc3e168ec96879b86e6eae840be7
Nuraina Najwa <nurainanajwa@graduate.utm.my>
2021-12-12 00:55:36 +0800

    commit

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,5 +1,25 @@
 // TODO Define the model class Todo here. The attributes are given. Complete the rest.
 
-// import 'task.dart';
+import 'task.dart';
 
-class Todo {}
+class Todo {
+    String title;
+    List<Task>tasks;
+
+    Todo({this.title, this.tasks});
+    Todo.copy(Todo from) : this(title: from.title, tasks...from.tasks]);
+
+    int totalTasks(){
+        return tasks.length;
+    }
+
+    int taskSCompleteness() {
+        int totalComplete = 0;
+        for (var task in tasks){
+            if (task.completed){
+                totalComplete++;
+            }
+        }
+    return ((totalComplete/tasks.length)*100).round();
+    }
+}
