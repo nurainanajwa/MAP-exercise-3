d395516281468e6fa65da79062a462a1f8b86af8
Nuraina Najwa <nurainanajwa@graduate.utm.my>
2021-12-12 01:09:00 +0800

    commit

--- lib/screens/task_list/body.dart
+++ lib/screens/task_list/body.dart
@@ -1,20 +1,45 @@
 // TODO Complete this file. This class should receive data from the TaskListScreen. The UI structure has been provided.
 //
-import 'package:flutter/material.dart';
 
-class Body extends StatelessWidget {
+import 'package:exercise2/models/task.dart';
+import 'package:flutter/material.dart';
+import '../../models/todo.dart';
+
+class Body extends StatefulWidget {
+  final Todo todoList;
+  Body(this.todoList);
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  var change = new List(20);
+  bool signal = false;
+
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
-      itemCount: 4,
+      itemCount: widget.todoList.tasks.length,
+      itemBuilder: (context, index) => ListTile(
+        title: widget.todoList.tasks[index].completed == false
+            ? Text(widget.todoList.tasks[index].title)
+            : Text(widget.todoList.tasks[index].title,
+                style: TextStyle(decoration: TextDecoration.lineThrough)),
+        onTap: () => setState(() {
+          widget.todoList.tasks[index] = Task(
+              title: widget.todoList.tasks[index].title,
+              completed: !widget.todoList.tasks[index].completed);
+          change[index] = true;
+          signal = true;
+        }),
+        onLongPress: () => setState(() {
+          widget.todoList.tasks.removeAt(index);
+        }),
+      ),
       separatorBuilder: (context, index) => Divider(
         color: Colors.blueGrey,
       ),
-      itemBuilder: (context, index) => ListTile(
-          title: Text('Task title',
-              style: TextStyle(decoration: TextDecoration.lineThrough)),
-          onTap: () {},
-          onLongPress: () {}),
     );
   }
 }
--- lib/screens/task_list/float.dart
+++ lib/screens/task_list/float.dart
@@ -1,8 +1,18 @@
 // TODO Complete this file. This class should receive data from the TaskListScreen.
 //
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import '../../models/todo.dart';
 
-class Float extends StatelessWidget {
+class Float extends StatefulWidget {
+  final Todo todoList;
+  Float(this.todoList);
+  
+  @override
+  State<Float> createState() => _FloatState();
+}
+
+class _FloatState extends State<Float> {
   @override
   Widget build(BuildContext context) {
     return Row(
