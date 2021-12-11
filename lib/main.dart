8d67ed703f2612b338ad256f6cdad63ad2b22c81
Nuraina Najwa <nurainanajwa@graduate.utm.my>
2021-12-12 01:49:49 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,7 @@
 //  _________    ___________   ___________    ________
 
 import 'package:flutter/material.dart';
-
-// import 'models/mock_todos.dart';
+import 'models/mock_todos.dart';
 import 'screens/todo_list/todo_list_screen.dart';
 // import 'screens/task_list/task_list_screen.dart'; //? Later you want to remove this line
 
@@ -24,7 +23,7 @@ void main() => runApp(
         title: 'MAP Exercise 3',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.purple),
-        home: TodoListScreen(),
+        home: TodoListScreen(myTodoList),
         // home: TaskListScreen(), //? uncomment this out to preview the second screen. Later, you want to remove this line
       ),
     );
