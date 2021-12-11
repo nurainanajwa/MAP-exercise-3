9305863ec19325ec0cc754048f0e3ccba77bc11a
Nuraina Najwa <nurainanajwa@graduate.utm.my>
2021-12-12 01:12:08 +0800

    commit

--- lib/screens/task_list/float.dart
+++ lib/screens/task_list/float.dart
@@ -7,12 +7,15 @@ import '../../models/todo.dart';
 class Float extends StatefulWidget {
   final Todo todoList;
   Float(this.todoList);
-  
+
   @override
   State<Float> createState() => _FloatState();
 }
 
 class _FloatState extends State<Float> {
+  var change = new List(20);
+  bool signal = false;
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -20,13 +23,17 @@ class _FloatState extends State<Float> {
       children: <Widget>[
         FloatingActionButton.extended(
           heroTag: null,
-          onPressed: () {},
+          onPressed: () => setState(() {
+            Navigator.pop(context, widget.todoList);
+          }),
           label: Text('Update'),
           icon: Icon(Icons.check_circle),
         ),
         FloatingActionButton.extended(
           heroTag: null,
-          onPressed: () {},
+          onPressed: () => setState(() {
+            Navigator.pop(context, widget.todoList);
+          }),
           label: Text('Cancel'),
           icon: Icon(Icons.cancel),
         ),
